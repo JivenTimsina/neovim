b05bb604e4b08a6ac60f3029a52f352a8c66fe78
Jiven Timsina <86583743+Jiventim@users.noreply.github.com>
2024-10-05 09:00:13 +0545

    Update mason.lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -18,18 +18,15 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-			})
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			--lspconfig.lua_ls.setup({
+			--	capabilities = capabilities,
+			--})
+			--lspconfig.clangd.setup({
+			--	capabilities = capabilities,
+			--})
+    --  lspconfig.html.setup({
+     --   capabilities = capabilities
+     -- })
 		end,
 	},
 }
